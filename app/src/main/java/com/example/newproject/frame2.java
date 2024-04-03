2d2a206a509c9251ebae2847ad4741d02c244817
Anastasia <thedevil_2@mail.ru>
2024-04-03 10:38:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/newproject/frame2.java
@@ -0,0 +1,14 @@
+package com.example.newproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frame2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame2);
+    }
+}
---
+++ app/src/main/java/com/example/newproject/frame5.java
@@ -0,0 +1,14 @@
+package com.example.newproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frame5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame5);
+    }
+}
